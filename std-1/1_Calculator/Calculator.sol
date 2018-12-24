c8031d7b2b6fb35602499eea0c0c3bc99157d8f7
app <aniketengg.225@gmail.com>
2018-12-24 18:19:55 +0530

    comments updated

--- std-1/1_Calculator/Calculator.sol
+++ std-1/1_Calculator/Calculator.sol
@@ -1,18 +1,43 @@
 pragma solidity ^0.5.0;
 
 contract Calculator {
+    
+    /**
+     * @dev Adds two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+    
     function add(int a, int b) public pure returns (int){
         return (a+b);
     }
     
+    /**
+     * @dev Subtracts two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
     function sub(int a, int b) public pure returns (int){
         return (a-b);
     }
     
+    /**
+     * @dev Multiplies two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
     function mul(int a, int b) public pure returns (int){
         return (a*b);
     }
     
+    /**
+     * @dev Divides two passed numbers
+     * @param a first number
+     * @param b second number
+     */
+
     function div(int a, int b) public pure returns (int){
         return (a/b);
     }
