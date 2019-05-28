048def1b4f9c3a8ab5e4f505a28770b91e11898c
Aniket <aniket@Anikets-MacBook-Air.local>
2019-05-29 01:27:25 +0530

    added Time

---
+++ std-1/6_Time/Time.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract Time {
+
+    /**
+     * @dev calculates future time
+     * @param time base time
+     */
+
+    function getTime(uint time) public view returns (uint){
+        if(time > now)
+            return time + 1 hours + 20 minutes + 30 seconds;
+        else
+            return 0;
+    }
+
+}
