ef30a69ecc4576875fcb18a9f6b621f21a9bed0d
aniket-engg <aniketengg.225@gmail.com>
2019-08-30 12:13:16 +0530

    lifecycle

---
+++ std-2/3_Lifecycle/Lifecycle.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+contract Lifecycle {
+
+    enum Stage{Infant, Toddler, Child, Teen, Adult, Old}
+
+    /**
+     * @dev Return stage of life
+     * @param rollmonthsnumber Number of months
+     */
+
+    function getStage(uint months) public pure returns (Stage){
+        if(months > 0 && months <= 12)
+            return Stage.Infant;
+        else if(months > 12 && months <= 24)
+            return Stage.Toddler;
+        else if(months > 24 && months <= 155)
+            return Stage.Child;
+        else if(months > 156 && months <= 228)
+            return Stage.Teen;
+        else if(months > 228 && months <= 720)
+            return Stage.Adult;
+        else
+            return Stage.Old;
+    }
+}
