6df708fb3c03287ef67f86e7cb60f366a9c964e9
Aniket <30843294+Aniket-Engg@users.noreply.github.com>
2022-04-28 11:42:48 +0530

    Update PrimeNumber.sol

--- std-1/4_PrimeNumber/PrimeNumber.sol
+++ std-1/4_PrimeNumber/PrimeNumber.sol
@@ -8,6 +8,7 @@ contract PrimeNumber {
      */
 
     function ifPrime(int num) public pure returns (bool){
+        if (num == 1) return false;
         for(int i = 2; i < num; i++){
             if(num % i == 0)
                 return false;
