4b6a7a253f2504a4d4cbd04334db67cc93a53c7e
Riju Chakraborty <88335700+RijuChakraborty@users.noreply.github.com>
2022-10-05 13:34:44 +0530

    NatSpec added in AttendenceRegisterExtended

--- std-3/2_AttendanceRegisterExtended/AttendanceRegisterExtended.sol
+++ std-3/2_AttendanceRegisterExtended/AttendanceRegisterExtended.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.17;
 
 import "../../std-2/5_AttendanceRegister/AttendanceRegister.sol";
 
+/** @title A contract for viewing students from Attendence register 
+*   @author Riju Chakraborty
+*   @notice This contract is inheriting from AttendenceRegister contract
+*/
+
 contract AttendanceRegisterExtended is AttendanceRegister{
 
+    /// @dev Function returns an object of type Student struct from Attendence Register
     function getStudentByRoll (uint256 _roll) public view returns (Student memory){
         return register[_roll];
     }
