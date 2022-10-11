7dbfd6e45c1c1869737f9bd273cac1704cf3dff0
Kali-Decoder <neerajchoubisa876@gmail.com>
2022-10-11 18:15:35 +0530

    Changing Library Defination

--- std-3/1_CalculatorUsingLib/CalculatorLibrary.sol
+++ std-3/1_CalculatorUsingLib/CalculatorLibrary.sol
@@ -6,9 +6,7 @@ pragma solidity >=0.7.0 <0.9.0;
 // sub(uint256 a, uint256 b) → uint256
 // mul(uint256 a, uint256 b) → uint256
 // div(uint256 a, uint256 b) → uint256
-// mod(uint256 a, uint256 b) → uint256
-// max(uint256 a, uint256 b) → uint256
-// min(uint256 a, uint256 b) → uint256
+
 
 // @Library Is Deployed On Polygon Testnet Address :- "0x84903e57A043d28B5e476dB3cF6F5D9193AF5000";
 
@@ -38,21 +36,5 @@ library MathLibrary {
         return num1 / num2;
     }
 
-    //For Find Maximum
-    function max(uint256 num1, uint256 num2) external pure returns (uint256) {
-        uint256 x = num1 >= num2 ? num1 : num2;
-        return x;
-    }
-
-    // For Find Minimum
-    function min(uint256 num1, uint256 num2) external pure returns (uint256) {
-        uint256 x = num1 >= num2 ? num2 : num1;
-        return x;
-    }
-
-    // For Find Mod
-    function mod(uint256 num1, uint256 num2) external pure returns (uint256) {
-        require(num1 == 0 || num2 == 0);
-        return num1 % num2;
-    }
+    
 }
