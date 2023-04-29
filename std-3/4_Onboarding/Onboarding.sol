40bd6dc80c657d852c0ebcb15fc8262e8d539298
Amar Fazlić <73897903+amar-exe@users.noreply.github.com>
2023-04-29 21:45:05 +0200

    Create Onboarding.sol
    - Create Onboarding abstract contract
    - Create Allocation contract
    - Override methods and perform next steps

---
+++ std-3/4_Onboarding/Onboarding.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+abstract contract Onboarding {
+
+    struct Employee {
+        string name;
+        string email;
+        uint256 salary;
+    }
+
+    Employee public employee;
+
+    function join(string memory _name, string memory _email, uint256 _salary) public virtual {
+        employee = Employee(_name, _email, _salary);
+    }
+
+    function nextSteps() public view virtual;
+}
+
+contract Allocation is Onboarding {
+
+    function allocate(string memory _employeeId, string memory _projectId) public view returns (string memory) {
+        string memory text = string.concat("The employee with id: ", _employeeId, ", is added to project with id: ", _projectId);
+        nextSteps();
+        return text;
+    }
+
+    function nextSteps() public view override {
+        // Perform necessary actions for allocation
+        sendWelcomeEmail();
+    }
+
+    function sendWelcomeEmail() internal view returns (string memory) {
+        return string.concat("Sent welcome email to employee with email: ", employee.email);
+    }
+}
